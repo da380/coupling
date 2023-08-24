4055a36ac57a7365219a05d35e4867ce0e25325f
Alex Myhill <adcm2@cam.ac.uk>
2023-08-24 17:28:03 +0100

    Everything working, some messing about

--- work/spectra_plot.py
+++ work/spectra_plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 d = np.loadtxt('spectra.STAT1.Z')
+dp = np.loadtxt('spectra_small.STAT1.Z')
 
 
 plt.rcParams.update({'font.size': 18})
@@ -9,6 +10,7 @@ plt.rcParams.update({'font.size': 18})
 plt.figure()
 
 plt.plot(d[:,0],d[:,3],'k')
+plt.plot(dp[:,0],dp[:,3],'r--')
 
 plt.gca().set_yticks([])
 plt.yticks([])
